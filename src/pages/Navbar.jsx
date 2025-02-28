be4e6dd35c74c618f410aa09eb04b713f07abb53
Vishwa2011 <vishwaitaliya@23gmail.com>
2025-02-28 16:04:06 +0530

    changes

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
               <div className="logo-header">
                 <div className="logo-header-inner logo-header-one">
                   <a href="/">
-                    <img src="/assets/pic/Craft_My_Space_Custom_C_Logo_180x31.png" alt="" />
+                    <img src="/assets/pic/CMS LOGO 1.png" alt="" />
                   </a>
                 </div>
               </div>
@@ -21,63 +21,20 @@ const Navbar = () => {
                 <span className="icon-bar bar-three"></span>
               </button>
               {/* <!-- ETRA Nav --> */}
-              {/* <div className="extra-nav">
-                <div className="extra-cell">
-                  <a href="#search" className=" text-white">
-                    <i className="fa fa-search"></i>
-                  </a>
-                </div>
-              </div> */}
-              {/* <!-- ETRA Nav --> */}
-
-              {/* <!-- SITE Search --> */}
-              {/* <div id="search">
-                <span className="close"></span>
-                <form
-                  role="search"
-                  id="searchform"
-                  action="/search"
-                  method="get"
-                  className="radius-xl"
-                >
-                  <div className="input-group">
-                    <input
-                      className="form-control"
-                      value=""
-                      name="q"
-                      type="search"
-                      placeholder="Type to search"
-                    />
-                    <span className="input-group-append">
-                      <button type="button" className="search-btn">
-                        <i className="sl-icon-magnifier"></i>
-                      </button>
-                    </span>
-                  </div>
-                </form>
-              </div> */}
+             
               {/* <!-- MAIN Vav --> */}
               <div className="header-nav">
                 <ul className=" nav navbar-nav">
-                  <li className="">
-                    <a href="../WhyCraftMySpace">Why CMS?</a>
-                    {/* <ul className="sub-menu">
-                                                <li>
-                                                    <a href="index.html">Home 1</a>
+                <li>
+                                                    <a href="/">Home</a>
+                                                
                                                 </li>
-                                                <li>
-                                                    <a href="index-2.html">Home 2</a>
-                                                </li>
-                                                <li>
-                                                    <a href="index-3.html">Home 3</a>
-                                                </li>
-                                                <li>
-                                                    <a href="index-4.html">Home 4</a>
-                                                </li>
-                                            </ul> */}
+                  <li>
+                    <a href="../WhyCraftMySpace">WhyCraftMySpace?</a>
+                   
                                         </li>
                                         <li>
-                                            <a href="">Fitted Furniture</a>
+                                            <a href="">Services</a>
                                             <ul className="sub-menu">
 
                                                 <li>
@@ -131,39 +88,28 @@ const Navbar = () => {
                                         </li>
                                         <li>
                                             <a href="/Joinery">Joinery</a>
-                                            {/* <ul className="sub-menu has-child">
-                                                <li><a href="news-grid.html">Blog Grid</a></li>
-                                                <li><a href="news-listing.html">Blog Listing</a></li>
-                                                <li><a href="news-masonry.html">Blog Masonry</a></li>
-                                            </ul> */}
+                                            
                   </li>
                   <li>
                     <a href="/Inspiration">Inspiration</a>
-                    {/* <ul className="sub-menu">
-                                                <li><a href="post-image.html">Post Image</a></li>
-                                                <li><a href="post-gallery.html">Post Gallery</a></li>
-                                                <li><a href="post-video.html">Post Video</a></li>
-                                                <li><a href="post-right-sidebar.html">Post Right Sidebar</a></li>
-                                            </ul> */}
+                    
                   </li>
 
                                         <li className="submenu-direction">
                                             <a href="">Gallery</a>
                                             <ul className="sub-menu">
                                                 <li><a href="/Recentwork">Recent Work</a></li>
-                                                {/* <li><a href="work-masonry.html">Project Masonry</a></li>
-                                                <li><a href="work-carousel.html">Project Carousel</a></li>
-                                                <li><a href="project-detail.html">Project Detail</a></li> */}
+                                                
                                             </ul>
                                         </li>
                                         <li className="submenu-direction">
-                                            <a href="">Get Quote</a>
+                                            <a href="">Contact</a>
                                             <ul className="sub-menu">
-                                                <li><a href="/Getaquote">Get A Quote</a></li>
+                                                <li><a href="/Getaquote">Contact Us</a></li>
                                                 <li><a href="/Orderafreebrochure">Order A Free Brochure</a></li>
                                                 <li><a href="/PrivacyPolicy">Privacy Policy</a></li>
                                                 <li><a href="/Termandcondition">Terms & Condition</a></li>
-                                                {/* <li><a href="project-detail.html">Project Detail</a></li> */}
+                                               
                                             </ul>
                                         </li>
                                     </ul>
